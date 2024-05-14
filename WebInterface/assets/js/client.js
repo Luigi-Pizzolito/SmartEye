02ad994c748134eaa8ace3b49aec48801624a28c
KaelKang <956136864@qq.com>
2024-05-14 21:30:50 +0800

    server receives images and actions from websocket and registers new faces

--- WebInterface/assets/js/client.js
+++ WebInterface/assets/js/client.js
@@ -6,6 +6,7 @@ const socket=new WebSocket("ws://localhost:8100")
 
 const faceReg=document.getElementById('faceReg')
 const faceSave=document.getElementById('faceSaved')
+const faceLoad=document.getElementById('faceLoad')
 
 faceReg.addEventListener('click',()=>{
     socket.send(JSON.stringify({action:'r'}));
@@ -15,10 +16,36 @@ faceSave.addEventListener('click',()=>{
     socket.send(JSON.stringify({action:'s'}));
 });
 
+faceLoad.addEventListener('click',()=>{
+    socket.send(JSON.stringify({action:'q'}));
+});
+
 navigator.mediaDevices.getUserMedia({video:true})
-.then((stream)=>{
-   const videoElement=document.getElementById('videoElement');
-   videoElement.srcObject=stream
+// .then((stream)=>{
+//    const videoElement=document.getElementById('videoElement');
+//    videoElement.srcObject=stream
+// })
+.then(function(stream) {
+    // -- transmit images to server via websocket
+    const videoElement=document.getElementById('videoElement');
+    videoElement.srcObject=stream;
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    video.play();
+
+    setInterval(function() {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        canvas.toBlob(function(blob) {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(blob);
+            }
+        }, 'image/jpeg', 0.9); // JPEG格式，质量为0.9
+    }, 1000 / 30); // 假设30fps
 })
 .catch((error)=>{
     socket.send(JSON.stringify({action:"ERROR"}))
